920c0f44e8759c194f740c413fe513dec176fab5
patryklyczko <lyczkopatryk1@gmail.com>
2023-02-26 22:05:36 +0100

    chore: added main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,37 @@
 package main
 
-import (
-	que "Algorithms/algorithms/queues.go"
-	"fmt"
-)
-
 func main() {
-	q := que.FIFOQueue()
-	q.Push(1)
-	q.push(2)
-	q.push(3)
-	fmt.Print(q.pop())
+
 }
+
+// graph := make(map[int]([]int))
+// 	graph[1] = []int{2, 3, 4, 5}
+// 	graph[2] = []int{3, 5}
+// 	graph[3] = []int{4}
+// 	graph[4] = []int{5}
+// 	s := 1
+// 	d := g.DFS(graph, s)
+// 	fmt.Print(d, "\n")
+// q := que.FIFOQueue()
+// q.Push(1)
+// q.Push(2)
+// q.Push(3)
+// fmt.Print(q.Pop(), "\n")
+// lin := linked.LinkedList()
+// lin.Append(3)
+// lin.Append(1)
+// lin.Append(5)
+// lin.Append(3)
+// lin.Append(10)
+// lin.Append(5)
+// lin.Append(2)
+// lin.Show()
+// lin.Prepend(7)
+// lin.Show()
+// lin.Delete(3)
+// lin.Show()
+// arr := sort.BubbleSort()
+// 	arr.Add([]interface{}{1, 2, 4, 5, 76, 8, 23, 34, 24, 45})
+// 	arr.Show()
+// 	arr.Sort()
+// 	arr.Show()
